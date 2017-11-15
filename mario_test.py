5b09c9fc936dc1642ce9293d9b55e0abebbbbb9b
Chris Messier <messiercr@gmail.com>
2017-11-15 15:18:27 -0500

    added gitignore

--- mario_test.py
+++
@@ -1,8 +0,0 @@
-import gym 
-env = gym.make('SuperMarioBros-1-1-v0')
-observation = env.reset()
-for _ in range(10000):
-    env.render()
-    action = env.action_space.sample() # your agent here (this takes random actions)
-    observation, reward, done, info = env.step(action)
-
