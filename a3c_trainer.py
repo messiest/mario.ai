386b632bbb260b5af987677087bc35f5a87f8c82
Chris <messiercr@gmail.com>
2018-11-26 14:00:21 -0500

    tried setting visible devices programmatically

--- a3c_trainer.py
+++ a3c_trainer.py
@@ -68,7 +68,7 @@ def main(args):
         debug()
     os.environ['OMP_NUM_THREADS'] = "1"
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    devices = ",".join([i for i in range(torch.cuda.device_count())])
+    devices = ",".join([str(i) for i in range(torch.cuda.device_count())])
     os.environ["CUDA_VISIBLE_DEVICES"] = devices
 
     print(devices)
