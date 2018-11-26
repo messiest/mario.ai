3a35ad696dfe6c4b0149ee7a74706b8fc08ef76b
Chris <messiercr@gmail.com>
2018-11-26 15:59:40 -0500

    added file to perform all of the package installations

---
+++ utils/aws-setup/aws-setup0.sh
@@ -0,0 +1,7 @@
+conda install pytorch-nightly==1.0.0.dev20181123 cuda92 -c pytorch
+conda install torchvision -c pytorch --no-deps
+
+pip install gym
+pip install gym-super-mario-bros
+pip install setuptools==40.5.0
+pip insall xvfbwrapper
