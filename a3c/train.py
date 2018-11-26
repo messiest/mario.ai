f0037cdc5a3473c66cdb9b9b4cdfe0e524d49201
Chris <messiercr@gmail.com>
2018-11-26 11:59:10 -0500

    proper testing for cuda assignment

--- a3c/train.py
+++ a3c/train.py
@@ -140,10 +140,11 @@ def train(rank, args, shared_model, counter, lock, optimizer=None, device='cpu',
             if torch.cuda.is_available():
                 delta_t.cuda()
 
-            print("gae", type(gae))
+            print("gae", type(gae), gae.is_cuda)
             print("args.gamma", type(args.gamma))
             print("args.tau", type(args.tau))
-            print("delta_t", type(delta_t))
+            print("delta_t", type(delta_t), gae.is_cuda)
+            assert gae.is_cuda == delta_t.is_cuda, "CUDA mismatch!"
 
             gae = gae * args.gamma * args.tau + delta_t
 
