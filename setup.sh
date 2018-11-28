075d29fa8696f83238deb56f0e12b0b123909613
Chris <messiercr@gmail.com>
2018-11-28 13:06:16 -0500

    updated the setup file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 conda install pytorch-nightly cuda92 -c pytorch
 conda install torchvision -c pytorch --no-deps
 
-pip install gym
-pip install gym-super-mario-bros
-pip install setuptools==40.5.0
-pip install xvfbwrapper
+pip install gym gym-super-mario-bros xvfbwrapper emoji setuptools==40.5.0
