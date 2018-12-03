f9ac56a82e14cc105f77297af67cd96f2eb8e678
Chris <messiercr@gmail.com>
2018-12-03 15:46:41 -0500

    adding loss.py to tracking

---
+++ a3c/loss.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn.functional as F
+
+
+def gae(R, rewards, values, log_probs, entropies, args):
+    """Generalized Advantage Estimation"""
+    policy_loss = 0
+    value_loss = 0
+    loss = torch.zeros(1, 1)
+    for i in reversed(range(len(rewards))):
+        if torch.cuda.is_available():
+            loss = loss.cuda()
+
+        R = args.gamma * R + rewards[i]
+        if torch.cuda.is_available():
+            R = R.cuda()
+
+        advantage = R - values[i]
+        value_loss = value_loss + 0.5 * advantage.pow(2)
+
+        delta_t = rewards[i] + args.gamma * values[i + 1].data - values[i].data
+        if torch.cuda.is_available():
+            delta_t = delta_t.cuda()
+
+        if torch.cuda.is_available():
+            loss = loss.cuda() * args.gamma * args.tau + delta_t.cuda()
+        else:
+            loss = loss.cpu() * args.gamma * args.tau + delta_t.cpu()
+
+        policy_loss = policy_loss - \
+                      log_probs[i] * loss - \
+                      args.entropy_coef * entropies[i]
+
+    total_loss = policy_loss + args.value_loss_coef * value_loss
+
+    return total_loss
