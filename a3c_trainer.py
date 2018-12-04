9e027070ca8aeab8bf7069658ba3ac8b2b6a3b0c
Chris <messiercr@gmail.com>
2018-12-04 14:51:58 -0500

    moved cli to separate file and changed save location for results

--- a3c_trainer.py
+++ a3c_trainer.py
@@ -13,46 +13,15 @@ from models import ActorCritic
 from optimizers import SharedAdam
 from mario_wrapper import create_mario_env
 from a3c import train, test
-from utils import FontColor, fetch_name, debug, restore_checkpoint
+from utils import FontColor, fetch_name, debug, restore_checkpoint, cli
 from mario_actions import ACTIONS
-
-
-# Command Line Interface
-parser = argparse.ArgumentParser(description='A3C')
-parser.add_argument('--lr', type=float, default=0.0001, help='learning rate (default: 0.0001)')
-parser.add_argument('--gamma', type=float, default=0.9, help='discount factor for rewards (default: 0.9)')
-parser.add_argument('--tau', type=float, default=1.00, help='parameter for GAE (default: 1.00)')
-parser.add_argument('--entropy-coef', type=float, default=0.01, help='entropy term coefficient (default: 0.01)')
-parser.add_argument('--value-loss-coef', type=float, default=0.5, help='value loss coefficient (default: 0.5)')
-parser.add_argument('--max-grad-norm', type=float, default=250, help='value loss coefficient (default: 250)')
-parser.add_argument('--seed', type=int, default=1, help='random seed (default: 1)')
-parser.add_argument('--num-processes', type=int, default=_mp.cpu_count(), help='how many training processes to use (default: 4)')
-parser.add_argument('--num-steps', type=int, default=50, help='number of forward steps in A3C (default: 50)')
-parser.add_argument('--max-episode-length', type=int, default=1000000, help='maximum length of an episode (default: 1000000)')
-parser.add_argument('--env-name', default='SuperMarioBrosNoFrameskip-v0', help='environment to train on (default: SuperMarioBrosNoFrameskip-v0)')
-parser.add_argument('--no-shared', default=False, help='use an optimizer without shared momentum.')
-parser.add_argument('--use-cuda', default=True, help='run on gpu.')
-parser.add_argument('--record', action='store_true', help='record playback of tests')
-parser.add_argument('--save-interval', type=int, default=10, help='model save interval (default: 10)')
-parser.add_argument('--non-sample', type=int, default=int(_mp.cpu_count() / 2), help='number of non sampling processes (default: 2)')
-parser.add_argument('--checkpoint-dir', type=str, default='checkpoints', help='directory to save checkpoints')
-parser.add_argument('--start-step', type=int, default=0, help='training step on which to start')
-parser.add_argument('--model-id', type=str, default=fetch_name(), help='name id for the model')
-parser.add_argument('--start-fresh', action='store_true', help='start training a new model')
-parser.add_argument('--load-model', default=None, type=str, help='model name to restore')
-parser.add_argument('--verbose', action='store_true', help='print actions for debugging')
-parser.add_argument('--debug', action='store_true', help='print versions of essential packages')
-parser.add_argument('--move-set', default='complex', type=str, help='the set of possible actions')
-parser.add_argument('--algorithm', default='A3C', type=str, help='algorithm being used')
-parser.add_argument('--headless', action='store_true', help='use virtual frame buffer')
-parser.add_argument('--reset-delay', type=int, default=60, help='delay between evaluations')
-parser.add_argument('--save-file', type=str, default='save/results.csv', help='file to save results to')
-
-args = parser.parse_args()
 
 
 # multiprocessing
 mp = _mp.get_context('spawn')
+
+# command line arguments
+args = cli.get_args()
 
 
 def main(args):
--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,4 @@ from utils.fonts import FontColor
 from utils.roster import fetch_name
 from utils.greedy_epsilon import get_epsilon
 from utils.transport import save_checkpoint, restore_checkpoint
+from utils.cli import get_args
